72f45420e279f870cc257ba829d1e71b67b3b866
Michael Zhong <mzhong@ideoorg-201504-060.local>
2017-12-10 16:07:23 -0800

    todo card styling

--- src/components/PageTodo.vue
+++ src/components/PageTodo.vue
@@ -1,7 +1,16 @@
 <template lang="jade">
 .container
+  .map_section
   .todo_section
-    TodoCard
+    TodoCard(v-for="card in cards",
+    :key="card.name",
+    :name="card.name",
+    :rating="card.rating",
+    :price="card.price",
+    :time="card.time",
+    :address="card.address",
+    :phone="card.phone",
+    :description="card.description")
 </template>
 
 <script>
@@ -11,16 +20,39 @@ export default {
   name: 'PageTodo',
   components: {
     TodoCard
+  },
+  data () {
+    return {
+      cards: [
+        {
+          name: 'Eis-Greissler',
+          rating: 'Must Must Do',
+          price: '$5 / scoop',
+          time: '5-30 min',
+          address: '1 Stubenplatz, Vienna',
+          phone: '1-432-714-9321',
+          description: 'Best ice cream in the world. Specializes in organic unique flavors, including pumpkin seed.'
+        },
+        {
+
+        }
+      ]
+    }
   }
-  // data () {
-  // }
 }
 </script>
 
 <style scoped lang="sass">
-  .container
-    height: 85%
-  .todo_section
-    margin-top: 45%
-    height: 55%
+.container
+  height: calc(100% - 56px)
+.map_section
+  height: 45%
+  background: rgb(248,248,248)
+.todo_section
+  height: 55%
+  width: 100000px
+  overflow: hidden
+  display: flex
+  align-items: center
+  padding-left: 1.5rem
 </style>
--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,15 +1,92 @@
 <template lang="jade">
 .todo_card
-  img(src="@/assets/logo.png")
+  .top_section
+    img(:src="logo").todo_img
+    .filter_section
+      p.label_tag RATING
+      p.label_info {{ rating }}
+      p.label_tag PRICE
+      p.label_info {{ price }}
+      p.label_tag TIME
+      p.label_info {{ time }}
+  .bottom_section
+    .place_section
+      .place_name_section
+        p.place_name {{ name }}
+      .place_info_section
+        p.place_address {{ address }}
+        p.place_phone {{ phone }}
+    p.description {{ description }}
 </template>
 
 <script>
+import logo from '@/assets/logo.png'
 export default {
-  name: 'TodoCard'
-  // data () {
-  // }
+  name: 'TodoCard',
+  props: ['name', 'rating', 'price', 'time', 'address', 'phone', 'description'],
+  data () {
+    return {
+      logo
+    }
+  }
 }
 </script>
 
 <style scoped lang="sass">
+.todo_card
+  width: 240px
+  height: 240px
+  box-shadow: 0 2px 6px rgba(113,113,113,0.5)
+  border-top: 3px solid rgba(144,19,255, 0.5)
+  margin-right: 2rem
+.todo_img
+  width: 57%
+  height: auto
+  border: 1px solid rgb(235,235,235)
+.top_section, .bottom_section
+  width: 90%
+  margin: 0 auto
+.top_section
+  display: flex
+  height: 60%
+  align-items: flex-start
+  padding-top: 5px
+.filter_section
+  height: 100%
+  width: 46%
+  padding-left: 3%
+  align-items: center
+.label_tag
+  letter-spacing: 0.2px
+  font-weight: 600
+  color: rgb(184,184,184)
+  font-size: 0.625rem
+  margin: 0.5rem 0 0.1rem 0
+.label_info
+  font-weight: 500
+  color: rgb(40,40,40)
+  font-size: 0.75rem
+  margin: 0
+.bottom_section
+  height: 40%
+.place_section
+  display: flex
+  align-items: flex-start
+.place_name_section
+  border-right: 1px solid rgb(230,230,230)
+  padding-right: 0.3rem
+.place_name
+  font-size: 1rem
+  font-weight: 600
+  color: rgb(30,29,30)
+  margin: 0
+.place_info_section
+  padding-left: 0.3rem
+.place_address, .place_phone
+  font-size: 0.5rem
+  margin: 0
+.description
+  color: rgb(115,115,115)
+  font-size: 0.6875rem
+
 </style>
